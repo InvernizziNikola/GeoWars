e4d4a84d5c5900b05cdaed9ea78aff53c85c21ed
Tanaxis <nikola_invernizzi@hotmail.com>
2016-11-07 11:14:57 +0100

    First upload

---
+++ GEOWARS_SOURCE/desktop/src/com/group17/geowars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.group17.geowars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.group17.geowars.GeoWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GeoWars(), config);
+	}
+}
