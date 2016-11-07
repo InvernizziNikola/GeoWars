acb9d2955ad07a09fc37da01a5be8c519a9d60d2
egoon97 <egon.de.baene@student.howest.be>
2016-11-07 15:01:55 +0100

    azpoerjpazoiejrpo
    azpoiejrpazeoirj

--- GEOWARS_SOURCE/core/src/com/group17/geowars/GeoWars.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/GeoWars.java
@@ -21,7 +21,7 @@ public class GeoWars extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-   //hhhhhhh
+   //amopjerzkpoazijerôizaejrpoiazjerpoijazeporijazeproijazeporjizaepoirjzaeoijrpiozae
 		batch.draw(img, 500, 1000);
 		batch.end();
 
