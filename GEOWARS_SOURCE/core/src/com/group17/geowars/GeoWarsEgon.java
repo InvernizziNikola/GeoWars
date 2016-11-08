d8c7360f8ed6acc4387cbaf282a863e3d7e57b17
egoon97 <egon.de.baene@student.howest.be>
2016-11-08 13:32:08 +0100

    egon's launchers

---
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/GeoWarsEgon.java
@@ -0,0 +1,51 @@
+package com.group17.geowars;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.group17.geowars.GameObjects.Geom;
+
+public class GeoWarsEgon extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	Geom geom;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+
+
+		// REMOVE: GEOM TEST
+		geom = new Geom(2);
+		System.out.println(geom.toString());
+		System.out.println(geom.getLoot().toString());
+	}
+
+	@Override
+	public void render () {
+
+		geom.update();
+
+
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+
+		geom.render(batch);
+
+		batch.draw(img, 150, 150);
+		batch.end();
+
+
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
