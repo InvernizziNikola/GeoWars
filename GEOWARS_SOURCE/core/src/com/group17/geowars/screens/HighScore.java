420cbc3d7bedae0174ffeeaf1e587fb2f9490df3
egoon97 <egon.de.baene@student.howest.be>
2016-11-10 16:23:26 +0100

    images and update highscore
    working highscore

--- GEOWARS_SOURCE/core/src/com/group17/geowars/screens/HighScore.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/screens/HighScore.java
@@ -1,16 +1,20 @@
 package com.group17.geowars.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.group17.geowars.database.DBManager;
+import com.group17.geowars.managers.Managers;
 
 import java.util.ArrayList;
 
@@ -36,10 +40,97 @@ public class HighScore {
 
 
     public HighScore() {
-        HighScore();
+        HighScore("arcade");
     }
 
-    public void HighScore() {
+    public void Buttons(){
+        // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+        // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+        skin = new Skin();
+        // Generate a 1x1 white texture and store it in the skin named "white".
+        Pixmap pixmap = new Pixmap(100, 100, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+
+        skin.add("white", new Texture(pixmap));
+
+        // Store the default libgdx font under the name "default".
+        BitmapFont bfont = new BitmapFont();
+
+        skin.add("default", bfont);
+
+        // Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+        final TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.WHITE);
+        textButtonStyle.fontColor = Color.BLACK;
+
+        textButtonStyle.font = skin.getFont("default");
+
+        skin.add("default", textButtonStyle);
+
+        final TextButton campaignButton = new TextButton("CAMPAIGN",textButtonStyle);
+        campaignButton.setPosition(125,400);
+        campaignButton.setHeight(50);
+        campaignButton.setWidth(150);
+        stage.addActor(campaignButton);
+
+        final TextButton arcadeButton = new TextButton("ARCADE",textButtonStyle);
+        arcadeButton.setPosition(325,400);
+        arcadeButton.setHeight(50);
+        arcadeButton.setWidth(150);
+        stage.addActor(arcadeButton);
+        arcadeButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+                System.out.println("CLICKED: " + actor);
+                //Screen nextMenu = Managers.getMenuManager().getScreen("playArcade");
+                //Managers.getMenuManager().setScreen(nextMenu);
+            }
+        });
+
+        final TextButton multiPlayerButton = new TextButton("CO-OP",textButtonStyle);
+        multiPlayerButton.setPosition(525,400);
+        multiPlayerButton.setHeight(50);
+        multiPlayerButton.setWidth(150);
+        stage.addActor(multiPlayerButton);
+
+        final TextButton backButton = new TextButton("BACK", textButtonStyle);
+        backButton.setPosition(325, 100);
+        backButton.setHeight(50);
+        backButton.setWidth(150);
+        stage.addActor(backButton);
+        /*--------------EVENT HANDLER--------------------------*/
+        campaignButton.addListener(new ChangeListener() {
+
+            public void changed(ChangeEvent event, Actor actor) {
+
+                System.out.println("CLICKED: " + actor);
+                HighScore("campaign");
+            }
+        });
+        multiPlayerButton.addListener(new ChangeListener() {
+
+            public void changed(ChangeEvent event, Actor actor) {
+
+                System.out.println("CLICKED: " + actor);
+                HighScore("coop");
+            }
+        });
+        arcadeButton.addListener(new ChangeListener() {
+
+            public void changed(ChangeEvent event, Actor actor) {
+                stage.clear();
+
+                System.out.println("CLICKED: " + actor);
+                HighScore("arcade");
+            }
+        });
+
+
+    }
+    public void HighScore(String GameMode) {
+
 /*    BitmapFont font = new BitmapFont();
         Label.LabelStyle style = new Label.LabelStyle();
         Label text;
@@ -51,9 +142,11 @@ public class HighScore {
         title = new BitmapFont();
         title.setColor(Color.WHITE);
         stage = new Stage();
+        stage.clear();
         Gdx.input.setInputProcessor(stage);
         skin = new Skin();
         skin1 = new Skin();
+        Buttons();
         Pixmap pixmap = new Pixmap(200, 50, Pixmap.Format.RGBA8888);
         pixmap.setColor(Color.WHITE);
         pixmap.fill();
@@ -73,7 +166,7 @@ public class HighScore {
 
         //DATABASE connectie arcade
         DBManager manager = new DBManager();
-        ArrayList highScores = manager.DBselectTOP10Highscore("arcade");
+        ArrayList highScores = manager.DBselectTOP10Highscore(GameMode);
         table.add(new Label("Name", style)).width(100);
         table.add(new Label("Score", style)).width(100);
         table.row();
