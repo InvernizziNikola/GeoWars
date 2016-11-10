a84a8403be91b9e8e7a88faa26cf11e9af6d7463
kevinLeplae <kevinleplaee@hotmail.com>
2016-11-10 13:13:15 +0100

    lets merge :dancer:

--- GEOWARS_SOURCE/core/src/com/group17/geowars/Managers/ScoreManager.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/Managers/ScoreManager.java
@@ -13,27 +13,31 @@ public class ScoreManager {
 
 
 
-    private Dictionary<Integer, EnemyLoot> lootTable;
+    private int score;
+    private Dictionary<Integer,EnemyLoot> lootTable;
 
     public ScoreManager ()
     {
-        lootTable = new Hashtable<Integer, EnemyLoot>();
-
-        // GET LOOTTABLE FROM DATABASE AND INSERT IN HASHTABLE
-        // HACK
-        lootTable.put(1, new EnemyLoot(1,1,1));
-        lootTable.put(2, new EnemyLoot(2,2,2));
-        lootTable.put(3, new EnemyLoot(3,3,3));
-        lootTable.put(4, new EnemyLoot(4,4,4));
+        init();
     }
-    public void init()
+
+
+    public void init() {
+        score=0;
+        lootTable = new Hashtable<Integer, EnemyLoot>();
+        lootTable.put(0,new EnemyLoot(100,10,1));
+
+    }
+
+    public int getScore(int id)
     {
+        return score;
 
     }
 
     public EnemyLoot getLoot(int id)
     {
-        return lootTable.get(id);
+        return lootTable.get(0);
         // GET DATA FROM Dictionary
     }
 }
