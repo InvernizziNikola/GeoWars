18b9150947d7a8c7fcf52dc5833cc4519cc6dd5c
Tanaxis <nikola_invernizzi@hotmail.com>
2016-11-10 13:22:48 +0100

    Update

--- GEOWARS_SOURCE/core/src/com/group17/geowars/managers/LevelManager.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/managers/LevelManager.java
@@ -33,7 +33,7 @@ public class LevelManager implements GOInterface {
         spawnLocations.add(new Vector2(100,500));
         spawnLocations.add(new Vector2(500,400));
         spawnLocations.add(new Vector2(300,600));
-        NrOfEnemys = 5000;
+        NrOfEnemys = 200;
 
         enemies.add("tank");
         enemies.add("something_else");
