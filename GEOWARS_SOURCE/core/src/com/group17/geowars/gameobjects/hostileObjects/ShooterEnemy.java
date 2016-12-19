957ba1588351b9227f56e71b943ccb6cb00413e1
kevinLeplae <kevinleplaee@hotmail.com>
2016-12-19 14:08:10 +0100

    enemy update (shoot and run away)

--- GEOWARS_SOURCE/core/src/com/group17/geowars/gameobjects/hostileObjects/ShooterEnemy.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/gameobjects/hostileObjects/ShooterEnemy.java
@@ -2,6 +2,7 @@ package com.group17.geowars.gameobjects.hostileObjects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+import com.group17.geowars.GeoWars;
 import com.group17.geowars.gameobjects.Bullet;
 import com.group17.geowars.gameobjects.GOInterface;
 import com.group17.geowars.gameobjects.hostileObjects.Enemy;
@@ -17,20 +18,18 @@ public class ShooterEnemy extends Enemy implements GOInterface {
     private boolean canShoot = true;
     private float timer = 0;
 
-    private Vector2 shootDir = new Vector2(0,0);
-    private Vector2 moveDir = new Vector2(0,0);
-    private Vector2 lookAt = new Vector2(0,0);
+    private Vector2 shootDir = new Vector2(0, 0);
+    private Vector2 moveDir = new Vector2(0, 0);
+    private Vector2 lookAt = new Vector2(0, 0);
 
 
-    public ShooterEnemy(Vector2 spawnLocation)
-    {
-        super("fighter",new Vector2(0,0));
+    public ShooterEnemy(Vector2 spawnLocation) {
+        super("fighter", new Vector2(0, 0));
     }
-    public void shoot()
-    {
-        System.out.println(canShoot);
-        if(canShoot) {
-            Managers.getBulletManager().addBullet(new Bullet(new Vector2(position), new Vector2(lookAt),false));
+
+    public void shoot() {
+        if (canShoot) {
+            Managers.getBulletManager().addBullet(new Bullet(new Vector2(position), new Vector2(lookAt), false));
             canShoot = false;
         }
     }
@@ -38,8 +37,8 @@ public class ShooterEnemy extends Enemy implements GOInterface {
     @Override
     public void update() {
 
-        if(!insidePlayingField) {
-            target = new Vector2(Gdx.graphics.getWidth() /2, Gdx.graphics.getHeight() / 2);
+        if (!insidePlayingField) {
+            target = new Vector2(GeoWars.WIDTH / 2, GeoWars.HEIGHT / 2);
             if (position.x > 1
                     && position.x < Gdx.graphics.getWidth() - 1
                     && position.y > 1
@@ -49,29 +48,38 @@ public class ShooterEnemy extends Enemy implements GOInterface {
                 target = Managers.getAccountManager().getAccounts().get(0).getPlayer().getShip().getPosition();
             }
         }
-        if(insidePlayingField) {
-            if (position.x < -offset || position.x > Gdx.graphics.getWidth() + offset)
+        if (insidePlayingField) {
+            if (position.x < -offset || position.x > GeoWars.WIDTH + offset)
                 direction.x *= -1;
-            if (position.y <= -offset || position.y > Gdx.graphics.getHeight() + offset)
+            if (position.y <= -offset || position.y > GeoWars.HEIGHT + offset)
                 direction.y *= -1;
         }
         timer += Gdx.graphics.getDeltaTime();
-        if(timer > 0.15f) {
-            timer %= 0.15f;
+        if (timer > 0.75f) {
+            timer %= 0.75f;
             canShoot = true;
         }
 
         Vector2 dist = new Vector2(target.x - getPosition().x, target.y - getPosition().y);
 
         //aggro
-        if (dist.len() < 500 || !insidePlayingField)
-        {
-            shoot();
-            lookAt = dist.nor();
 
-        }else
-        {
+
+        if (dist.len() < 500 || !insidePlayingField) {//TODO remove ISinPlayingField
+            lookAt = new Vector2(dist).nor();
+
+                shoot();
+
+            }
+                System.err.println(dist.len());
+                System.out.println("get back");
+
+            }
+
+        } else {
             lookAt = direction.nor();
+
+
         }
         position.mulAdd(lookAt.nor(), 125 * Gdx.graphics.getDeltaTime());
     }
