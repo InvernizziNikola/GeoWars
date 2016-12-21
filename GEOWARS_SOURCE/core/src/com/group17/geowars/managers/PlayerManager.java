08dc4eeedfe49ce306b9ada87fb902dd5c78e8c6
Tanaxis <nikola_invernizzi@hotmail.com>
2016-12-21 10:53:31 +0100

    fix :+1:

--- GEOWARS_SOURCE/core/src/com/group17/geowars/managers/PlayerManager.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/managers/PlayerManager.java
@@ -1,27 +1,12 @@
 package com.group17.geowars.managers;
 
-/**
- * Created by nikola on 21/12/2016.
- */
 
-import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.group17.geowars.playerobjects.Account;
-
-import java.util.ArrayList;
-import java.util.List;package com.group17.geowars.managers;
-
-import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.group17.geowars.playerobjects.Account;
 import com.group17.geowars.playerobjects.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by nikola on 10/11/2016.
- */
 public class PlayerManager {
 
     private List<Player> players;
@@ -43,12 +28,21 @@ public class PlayerManager {
 
     public void update()
     {
+        for(Player p: players)
+        {
+            p.update();
+        }
     }
     public void render(Batch batch)
     {
+        for(Player p : players)
+        {
+            p.render(batch);
+        }
     }
     public void reset()
     {
+        players.clear();
     }
 }
 
