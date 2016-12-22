5aefec42b8941717da57c3009ffde45593dbc109
Tanaxis <nikola_invernizzi@hotmail.com>
2016-12-22 15:55:14 +0100

    Warpgates

---
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/gameobjects/WarpGate.java
@@ -0,0 +1,105 @@
+package com.group17.geowars.gameobjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.group17.geowars.DataObjects.EnemyProfile;
+import com.group17.geowars.managers.Managers;
+import com.group17.geowars.utils.WARPSTATE;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by nikola on 22/12/2016.
+ */
+
+public class WarpGate {
+
+    private Texture texture;
+    private Sprite sprite;
+    private float size;
+    private float maxSize;
+    private float angle;
+    private float rotateSpeed;
+    private Vector2 position;
+    private WARPSTATE state;
+    private List<EnemyProfile> enemiesToSpawn;
+    private Random rand = new Random();
+    private float timer = 200;
+
+    public WarpGate(Vector2 position, List<EnemyProfile> enemiesToSpawn)
+    {
+        this.position = position;
+        size = 0;
+        this.enemiesToSpawn = enemiesToSpawn;
+        maxSize = 200;
+        rotateSpeed = 0.5f;
+        state = WARPSTATE.LOADING;
+
+        texture = Managers.getAssetManager().getTexture("warp");
+        sprite = new Sprite(texture, texture.getWidth(), texture.getHeight());
+    }
+
+    public void update()
+    {
+        switch(state)
+        {
+            case LOADING:
+            {
+                size+=2;
+                angle -= rotateSpeed*10;
+                if(size >= maxSize)
+                {
+                    size=maxSize;
+                    state = WARPSTATE.SPAWNING;
+                }
+                break;
+            }
+            case SPAWNING:
+            {
+                angle -= rotateSpeed*2;
+                spawnEnemies();
+                break;
+            }
+            case UNLOADING:
+            {
+                size-=3;
+                angle -= rotateSpeed*10;
+                if(size <= 0)
+                {
+                    size=0;
+                }
+                System.out.println("removing warpgate");
+                break;
+            }
+        }
+    }
+
+    public void spawnEnemies()
+    {
+        ArrayList<EnemyProfile> toRemove = new ArrayList<EnemyProfile>();
+        for(EnemyProfile ep : enemiesToSpawn)
+        {
+            // SPAWN ENEMY
+            float x = rand.nextFloat() % 200;
+            float y = rand.nextFloat() % 200;
+
+            Managers.getEnemyManager().spawnEnemy(ep, new Vector2(x, y)) ;
+            toRemove.add(ep);
+        }
+        enemiesToSpawn.removeAll(toRemove);
+        toRemove.clear();
+    }
+    public void render(Batch batch)
+    {
+        //sprite.setColor(color);
+        sprite.setSize(size, size);
+        sprite.setOrigin(size / 2, size / 2);
+        sprite.setRotation(angle);
+        sprite.setPosition(position.x - size / 2, position.y - size / 2);
+        sprite.draw(batch);
+    }
+}
--- GEOWARS_SOURCE/core/src/com/group17/geowars/managers/LevelManager.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/managers/LevelManager.java
@@ -4,34 +4,92 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.group17.geowars.DataObjects.EnemyProfile;
 import com.group17.geowars.GeoWars;
-import com.group17.geowars.gameobjects.GOInterface;
+import com.group17.geowars.gameobjects.WarpGate;
+import com.group17.geowars.utils.ENEMYTYPE;
+import com.sun.org.apache.xml.internal.security.keys.content.KeyValue;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by kevin on 9/11/2016.
  */
-public class LevelManager implements GOInterface {
 
-    private List<String> enemies;
-    private List<Vector2> spawnLocations;
-    private List<Integer> waveList;
-    private Integer currentwave;
+public class LevelManager {
+
+    private List<WarpGate> warpGates;
+    private int currentwave;
+    private int currentLevel;
     private BitmapFont font;
-    private Texture texture;
-    private Sprite sprite;
-
+    private Texture textureBG;
+    private Sprite spriteBG;
 
 
     public LevelManager () {
-        enemies = new LinkedList<String>();
-        spawnLocations= new ArrayList<Vector2>();
-        waveList = new ArrayList<Integer>();
-        currentwave=1;
+
+        warpGates = new ArrayList<WarpGate>();
+        currentwave = 1;
+        currentLevel = 0;
+    }
+    public void init()
+    {
+        font = new BitmapFont();
+
+        //backgound stuff
+        textureBG = Managers.getAssetManager().getTexture("test_background_geoWars");
+        spriteBG = new Sprite(textureBG, GeoWars.WIDTH, GeoWars.HEIGHT);
+    }
+
+    public void newWave()
+    {
+        int points = currentLevel * 30 + currentwave * 5;
+        int warpGateCount = MathUtils.ceil(points/35.0f);
+
+        Random rand = new Random();
+        List<EnemyProfile> possibleEnemies = Managers.getEnemyManager().getProfiles();
+
+        List<EnemyProfile> enemiesToWarp = new ArrayList<EnemyProfile>();
+
+        while(points > 0)
+        {
+            EnemyProfile ep = possibleEnemies.get(rand.nextInt(possibleEnemies.size()));
+            if(ep.type != ENEMYTYPE.BOSS)
+            {
+                points -= ep.difficultyGrade;
+                enemiesToWarp.add(ep);
+            }
+        }
+
+        warpGates.add(new WarpGate(new Vector2(200,200), enemiesToWarp));
+    }
+
+    public void render(Batch batch) {
+
+        spriteBG.draw(batch);
+        font.draw(batch, "Wave " + Managers.getLevelManager().getCurrentwave(), GeoWars.WIDTH/2, GeoWars.HEIGHT-50);
+
+        for(WarpGate wg : warpGates)
+            wg.render(batch);
+    }
+
+    public void update() {
+        for(WarpGate wg : warpGates)
+            wg.update();
+    }
+
+
+    public Integer getCurrentwave() {
+        return currentwave;
+    }
+    public void setCurrentwave(Integer currentwave) {
+        this.currentwave = currentwave;
     }
 
     public void reset()
@@ -39,67 +97,5 @@ public class LevelManager implements GOInterface {
         currentwave = 0;
     }
 
-    public void init()
-    {
-        font = new BitmapFont();
-        spawnLocations.add(new Vector2(GeoWars.WIDTH-100,GeoWars.HEIGHT-100));
-        spawnLocations.add(new Vector2(GeoWars.WIDTH-100,GeoWars.HEIGHT-200));
-        spawnLocations.add(new Vector2(GeoWars.WIDTH-200,GeoWars.HEIGHT-100));
-        spawnLocations.add(new Vector2(GeoWars.WIDTH-10,GeoWars.HEIGHT-50));
-        spawnLocations.add(new Vector2(GeoWars.WIDTH-10,GeoWars.HEIGHT-150));
-
-        waveList.add(10);
-        enemies.add("tank");
-        enemies.add("something_else");
-        enemies.add("fighter");
-
-        //backgound stuff
-        texture = Managers.getAssetManager().getTexture("test_background_geoWars");
-        sprite = new Sprite(texture, GeoWars.WIDTH, GeoWars.HEIGHT);
-
-    }
-    public void addEnemy(String enemy)
-    {
-        enemies.add(enemy);
-    }
-    public void addSpawnLocations(Vector2 location)
-    {
-        spawnLocations.add(location);
-    }
-
-    public List<String> getEnemies() {
-        return enemies;
-    }
-    public void clearSpawn() {
-        enemies.clear();
-    }
-
-    public List<Vector2> getSpawnLocations() {
-        return spawnLocations;
-    }
-    public List<Integer> getWaveList ()
-    {
-        return waveList;
-    }
-
-    public Integer getCurrentwave() {
-        return currentwave;
-    }
-
-    public void setCurrentwave(Integer currentwave) {
-        this.currentwave = currentwave;
-    }
-
-    @Override
-    public void render(Batch batch) {
-
-        sprite.draw(batch);
-        font.draw(batch, "Wave "+Managers.getLevelManager().getCurrentwave(), GeoWars.WIDTH/2, GeoWars.HEIGHT-50);
-    }
-
-    @Override
-    public void update() {
-
-    }
 
 }
