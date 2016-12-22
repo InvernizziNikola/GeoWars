0506bcaba3d4c4339325f2ec8ab8a6c6461dbe0f
Tanaxis <nikola_invernizzi@hotmail.com>
2016-12-22 13:01:03 +0100

    enemy profile :facepunch:

---
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/DataObjecten/EnemyProfile.java
@@ -0,0 +1,28 @@
+package com.group17.geowars.DataObjecten;
+
+import com.group17.geowars.utils.ENEMYTYPE;
+
+/**
+ * Created by nikola on 22/12/2016.
+ */
+public class EnemyProfile {
+
+    public String name;
+    public ENEMYTYPE type;
+    public String imageName;
+    public int health;
+    public float fireDelay;
+    public float fireRange;
+    public float speed;
+
+    public EnemyProfile(String name, String type, String imageName, int health, float fireDelay, float fireRange, float speed)
+    {
+        this.name = name;
+        this.type = ENEMYTYPE.valueOf(type);
+        this.imageName = imageName;
+        this.health = health;
+        this.fireDelay = fireDelay;
+        this.fireRange = fireRange;
+        this.speed = speed;
+    }
+}
