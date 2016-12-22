303fa6aac85080de815bf95ddc637a2d84410231
Tanaxis <nikola_invernizzi@hotmail.com>
2016-12-22 14:28:53 +0100

    TYPES AND GATES

---
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/utils/ENEMYTYPE.java
@@ -0,0 +1,11 @@
+package com.group17.geowars.utils;
+
+/**
+ * Created by nikola on 22/12/2016.
+ */
+public enum ENEMYTYPE {
+    FIGHTER,
+    KAMIKAZE,
+    DREADNOUGHT,
+    BOSS
+}
---
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/utils/WARPSTATE.java
@@ -0,0 +1,10 @@
+package com.group17.geowars.utils;
+
+/**
+ * Created by nikola on 22/12/2016.
+ */
+public enum WARPSTATE {
+    LOADING,
+    SPAWNING,
+    UNLOADING
+}
