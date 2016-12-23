baed9635f67892230c3febf194c11249e9b83835
Tanaxis <nikola_invernizzi@hotmail.com>
2016-12-23 15:55:09 +0100

    extended image button :kimono:

---
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/utils/ExtendeImageButton.java
@@ -0,0 +1,30 @@
+package com.group17.geowars.utils;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * Created by nikola on 06/12/2016.
+ */
+public class ExtendeImageButton extends ImageButton {
+
+    ImageButtonStyle defaultStyle;
+    ImageButtonStyle overStyle;
+
+    public ExtendeImageButton(ImageButtonStyle defaultStyle, ImageButtonStyle overStyle) {
+        super(defaultStyle);
+        this.defaultStyle = defaultStyle;
+        this.overStyle = overStyle;
+    }
+
+    public void setDefaultOver()
+    {
+        super.setStyle(defaultStyle);
+    }
+    public void setHoverStyle()
+    {
+        super.setStyle(overStyle);
+    }
+}
