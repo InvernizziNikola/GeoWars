a3cf11baf85c2735b8265218f58aa06444afa655
michieldespiegelaere <michiel.despiegelaere@student.howest.be>
2017-01-27 16:46:09 +0100

    shopMenu2

--- GEOWARS_SOURCE/core/src/com/group17/geowars/screens/ShopMenuScreen2.java
+++ GEOWARS_SOURCE/core/src/com/group17/geowars/screens/ShopMenuScreen2.java
@@ -21,13 +21,24 @@ import java.util.ArrayList;
  */
 public class ShopMenuScreen2 extends MenuScreen implements iHasStage, iSetActive {
 
-    private BitmapFont text;
-    private Batch batch;
-    private int coins = 1000;
+
+    protected Batch batch;
+    protected int coins = 1000;
+    protected int max;
+    protected int extraFirePower = 0;
+    protected int extraLife = 0;
+    protected Color color;
+    protected BitmapFont bigFont;
+    protected int extraSpeed;
+    protected int price;
+    protected int shield;
+
 
     public ShopMenuScreen2() {
         super();
         create();
+        color = Color.WHITE;
+        bigFont = Managers.getAssetManager().getGameFont(color, 25);
     }
 
     public void create() {
@@ -39,7 +50,7 @@ public class ShopMenuScreen2 extends MenuScreen implements iHasStage, iSetActive
     }
 
     public void createButtons() {
-        final ImageButton backButton = newImageButton("Menu_backicon", GeoWars.WIDTH - GeoWars.WIDTH / 6, 20, 150, 50, new MenuGrid(1, 2));
+        final ImageButton backButton = newImageButton("Menu_backicon", GeoWars.WIDTH - GeoWars.WIDTH / 6, 20, 150, 50, new MenuGrid(1, 3));
         backButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -49,50 +60,91 @@ public class ShopMenuScreen2 extends MenuScreen implements iHasStage, iSetActive
             }
         });
 
-        final ImageButton extraLifeButton = newImageButton("extra_life", 50, GeoWars.HEIGHT-100, 250, 50, new MenuGrid(0, 0));
+        final ImageButton extraLifeButton = newImageButton("extra_life", 50, GeoWars.HEIGHT - 100, 250, 50, new MenuGrid(0, 0));
         extraLifeButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                buy(500);
+                max = 1;
+                price = 500;
+                if (price < coins) {
+                    if (extraLife < max) {
+                        buy(price);
+                        extraLife++;
+                    }
+                }
             }
         });
 
-        final ImageButton shieldButton = newImageButton("shield", 50, GeoWars.HEIGHT-250, 250, 50, new MenuGrid(0, 1));
+        final ImageButton shieldButton = newImageButton("shield", 50, GeoWars.HEIGHT - 250, 250, 50, new MenuGrid(0, 1));
         shieldButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                buy(450);
+                price = 450;
+                max = 1;
+                if(price < coins)
+                {
+                    if (shield < max)
+                    {
+                        buy(price);
+                        shield++;
+                    }
+                }
             }
         });
 
-        final ImageButton extraFirePowerButton = newImageButton("extra_fire_power", 50, GeoWars.HEIGHT - GeoWars.HEIGHT/4 - GeoWars.HEIGHT/6, 250, 50, new MenuGrid(0, 2));
+        final ImageButton extraFirePowerButton = newImageButton("extra_fire_power", 50, GeoWars.HEIGHT - GeoWars.HEIGHT / 4 + 50 - GeoWars.HEIGHT / 6, 250, 50, new MenuGrid(0, 2));
         extraFirePowerButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                buy(400);
+                max = 1;
+                price = 400;
+                if (price < coins) {
+                    if (extraFirePower < max) {
+                        buy(price);
+                        extraFirePower ++;
+                    }
+                }
             }
         });
+
+        final ImageButton extraSpeedButton = newImageButton("extra_speed", 50, GeoWars.HEIGHT / 2, 250, 50, new MenuGrid(0, 3));
+        extraSpeedButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                max = 1;
+                price = 350;
+                if (price < coins) {
+                    if (extraSpeed < max) {
+                        buy(price);
+                        extraSpeed ++;
+                    }
+                }
+            }
+        });
+
+        final ImageButton currencyButton = newImageButton("credit ;)", GeoWars.WIDTH - 250, GeoWars.HEIGHT - 85, 50, 50, new MenuGrid(1, 0));
     }
 
-    public void showText()
-    {
-        text.draw(batch, ""+coins, GeoWars.WIDTH-50, GeoWars.HEIGHT-50);
-        text.draw(batch, "price: 500", GeoWars.WIDTH/4, GeoWars.HEIGHT-75);
-        text.draw(batch, "price: 450", GeoWars.WIDTH/4, GeoWars.HEIGHT - 225);
-        text.draw(batch, "price: 400", GeoWars.WIDTH/4, GeoWars.HEIGHT - GeoWars.HEIGHT/4 - GeoWars.HEIGHT/6+25);
+    public void showText() {
+        bigFont.draw(batch, "" + coins, GeoWars.WIDTH - 150, GeoWars.HEIGHT - 50);
+        bigFont.draw(batch, "price 500 current " + extraLife, GeoWars.WIDTH / 4, GeoWars.HEIGHT - 65);
+        bigFont.draw(batch, "price 450 current " + shield, GeoWars.WIDTH / 4, GeoWars.HEIGHT - 215);
+        bigFont.draw(batch, "price 400 current " + extraFirePower, GeoWars.WIDTH / 4, GeoWars.HEIGHT - GeoWars.HEIGHT / 4 - GeoWars.HEIGHT / 6 + 85);
+        bigFont.draw(batch, "price 350 current " + extraSpeed, GeoWars.WIDTH / 4, GeoWars.HEIGHT / 2 + 35);
     }
 
 
-    public int buy(int price)
-    {
-        if(coins < price)
-        {
+    public int buy(int price) {
+
+        if (coins < price) {
             return coins;
-        }else {
+        } else {
             coins -= price;
             return coins;
         }
+
     }
+
     @Override
     public void setActive() {
         if (active)
